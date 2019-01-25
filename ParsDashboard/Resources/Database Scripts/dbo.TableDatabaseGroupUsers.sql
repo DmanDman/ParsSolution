38061b18ab57d138a03006f94d8da79bd03a9ccb
Dwayne Sawyer <dmandman2@gmail.com>
2019-01-24 23:43:22 -0500

    New database tables for security

---
+++ ParsDashboard/Resources/Database Scripts/dbo.TableDatabaseGroupUsers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DatabaseGroupUsers] (
+    [DatabaseGroupUsersId] BIGINT     IDENTITY (1, 1) NOT NULL,
+    [DatabaseGroupID]	   NCHAR(10) NOT NULL, 
+	[DoctorID]             NCHAR (10) NULL,
+    [UserID]               NCHAR (10) NULL,
+    [RoleID] NCHAR(10) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([DatabaseGroupUsersId] ASC)
+);
+
